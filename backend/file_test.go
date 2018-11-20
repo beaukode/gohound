9725c479370890b25f6863c8fd2c91675d229e75
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2018-11-20 18:26:42 +0100

    Common interface for backends

--- backend/file_test.go
+++ backend/file_test.go
@@ -15,8 +15,6 @@
 package backend_test
 
 import (
-	"time"
-
 	"github.com/beaukode/gohound/backend"
 	. "gopkg.in/check.v1"
 )
@@ -50,71 +48,23 @@ func (s *FileSuite) TestNewFileMissingFile(c *C) {
 }
 
 func (s *FileSuite) TestGetNextTodo(c *C) {
-	todo, err := s.getBackend().GetNextTodo(10)
-	c.Assert(err, IsNil)
-	c.Assert(todo, HasLen, 4)
-	c.Assert(todo[0].Probetype, Equals, "tcp-connect")
-	c.Assert(todo[1].Probetype, Equals, "http-response")
-	c.Assert(todo[2].Probetype, Equals, "tcp-connect")
-	c.Assert(todo[3].Probetype, Equals, "tcp-connect")
+	base := interfaceTests{backend: s.getBackend()}
+	base.TestGetNextTodo(c)
 }
 
 func (s *FileSuite) TestGetNextTodoUseLimit(c *C) {
-	todo, err := s.getBackend().GetNextTodo(1)
-	c.Assert(err, IsNil)
-	c.Assert(todo, HasLen, 1)
+	base := interfaceTests{backend: s.getBackend()}
+	base.TestGetNextTodoUseLimit(c)
 }
 
 func (s *FileSuite) TestGetNextPreventConcurrentAccess(c *C) {
-	fbe := s.getBackend()
-	// Consume 1st probe
-	todo, err := fbe.GetNextTodo(1)
-	c.Assert(err, IsNil)
-	c.Assert(todo, HasLen, 1)
-	c.Assert(todo[0].Probetype, Equals, "tcp-connect")
-
-	// Consume next 3 probes
-	todo, err = fbe.GetNextTodo(3)
-	c.Assert(err, IsNil)
-	c.Assert(todo, HasLen, 3)
-	c.Assert(todo[0].Probetype, Equals, "http-response")
-
-	// No more probe
-	todo, err = fbe.GetNextTodo(10)
-	c.Assert(err, IsNil)
-	c.Assert(todo, HasLen, 0)
+	base := interfaceTests{backend: s.getBackend()}
+	base.TestGetNextPreventConcurrentAccess(c)
 }
 
 func (s *FileSuite) TestUpdate(c *C) {
-	fbe := s.getBackend()
-
-	todo, err := fbe.GetNextTodo(10)
-	c.Assert(err, IsNil)
-	c.Assert(todo, HasLen, 4)
-
-	probe := todo[0]
-	firstLockuid := probe.Lockuid
-	firstLocktime := probe.Locktime
-	firstProbetype := probe.Probetype
-
-	probe.Lockuid = ""
-	probe.Locktime = time.Time{}
-	fbe.Update(probe)
-
-	// Interval not elapsed
-	todo, err = fbe.GetNextTodo(10)
-	c.Assert(err, IsNil)
-	c.Assert(todo, HasLen, 0)
-
-	time.Sleep(time.Second)
-
-	// Interval elapsed probe must came back with new lock uid & time
-	todo, err = fbe.GetNextTodo(10)
-	c.Assert(err, IsNil)
-	c.Assert(todo, HasLen, 1)
-	c.Assert(todo[0].Lockuid, Not(Equals), firstLockuid)
-	c.Assert(todo[0].Locktime, Not(Equals), firstLocktime)
-	c.Assert(todo[0].Probetype, Equals, firstProbetype)
+	base := interfaceTests{backend: s.getBackend()}
+	base.TestUpdate(c)
 }
 
 func (s *FileSuite) getBackend() *backend.File {
